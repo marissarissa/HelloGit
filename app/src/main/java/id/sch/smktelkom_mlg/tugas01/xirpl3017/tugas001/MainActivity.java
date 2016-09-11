8a1886d730449dc73bb77ce6205034f8fa522a34
Marissa Nur Aini <marissarissarissa19@gmail.com>
2016-09-11 19:58:21 +0700

    .Commit Keempat

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3017/tugas001/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3017/tugas001/MainActivity.java
@@ -5,102 +5,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
-    EditText etNama;
-    Button bOk;
-    Spinner spAsal, spTujuan;
-    /*  RadioButton rbPP,rbSatu; */
-    RadioGroup rgBerangkat;
-    CheckBox lansia, dewasa, anak;
+public class MainActivity extends AppCompatActivity {
+    EditText nama;
     TextView tvHasil;
-
-
+    Spinner asal, tujuan;
+    RadioGroup rgstatus;
+    CheckBox lansia, dewasa, anak;
+    Button buttonOK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        etNama = (EditText) findViewById(R.id.editTextNama);
-        bOk = (Button) findViewById(R.id.button);
-        spAsal = (Spinner) findViewById(R.id.spinnerasal);
-        spTujuan = (Spinner) findViewById(R.id.spinnertujuan);
-        /* rbPP = (RadioButton) findViewById(R.id.radioButtonPP);
-        rbSatu = (RadioButton) findViewById(R.id.radioButtonSatu); */
-        rgBerangkat = (RadioGroup) findViewById(R.id.rgBerangkat);
+        nama = (EditText) findViewById(R.id.editTextNama);
+        asal = (Spinner) findViewById(R.id.spinnerasal);
+        tujuan = (Spinner) findViewById(R.id.spinnertujuan);
         lansia = (CheckBox) findViewById(R.id.checkBoxLansia);
         dewasa = (CheckBox) findViewById(R.id.checkBoxDewasa);
         anak = (CheckBox) findViewById(R.id.checkBoxAnak);
-        lansia.setOnCheckedChangeListener(this);
-        dewasa.setOnCheckedChangeListener(this);
-        anak.setOnCheckedChangeListener(this);
-
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
-        bOk.setOnClickListener(new View.OnClickListener() {
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                doProcess();
                 doClick();
             }
         });
     }
 
     private void doClick() {
-        String hasil = null;
-        int startlen = hasil.length();
-        if (lansia.isChecked()) hasil += lansia.getText() + "\n";
-        if (dewasa.isChecked()) hasil += dewasa.getText() + "\n";
-        if (anak.isChecked()) hasil += anak.getText() + "\n";
-        if (hasil.length() == startlen) hasil += "tidak ada pilihan";
-
-        if (rgBerangkat.getCheckedRadioButtonId() != -1) {
-            RadioButton rb = (RadioButton)
-                    findViewById(rgBerangkat.getCheckedRadioButtonId());
-            hasil = rb.getText().toString();
-
-        /*if(rbPP.isChecked()) {
-            hasil = rbPP.getText().toString();
-        }
-        else (rbSatu.isChecked()) {
-            hasil = rbSatu.getText().toString();*/
-        }
-
-
-        tvHasil.setText("Nama Anda " + etNama + " pesawat asal " + spAsal + " dengan tujuan " + spTujuan);
-    }
-
-    private void doProcess() {
-        if (isValid()) {
-            String nama = etNama.getText().toString();
-
-
-        }
-    }
-
-    private boolean isValid() {
-        boolean valid = true;
-
-        String nama = etNama.getText().toString();
-
-        if (nama.isEmpty()) {
-            etNama.setError("Nama belum diisi");
-            valid = false;
-        } else if (nama.length() < 3) {
-            etNama.setError("Nama minimal 3 karakter");
-        } else {
-            etNama.setError(null);
-        }
-        return valid;
-    }
-
-    @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean b) {
+        String Umur = " untuk usia";
+        if (lansia.isChecked()) Umur += " " + lansia.getText() + " ";
+        if (anak.isChecked()) Umur += " " + anak.getText() + " ";
+        if (dewasa.isChecked()) Umur += " " + dewasa.getText() + " ";
+        /*RadioButton rb = (RadioButton) findViewById(rgstatus.getCheckedRadioButtonId());*/
+        tvHasil.setText("Nama anda " + nama.getText().toString() + " dari " + asal + " menuju " + tujuan + " keberangkatan " /*+ rb.getText().toString()*/ + Umur);
     }
 }
